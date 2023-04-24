37913c53c772349458d2d8c8efe5bc548a535bf3
Michael Bestas <mkbestas@lineageos.org>
2023-04-24 22:06:35 +0300

    rhode: Set correct screen density
    Change-Id: Ia6505e0e4e918bfb173bcf15fd7941988213f5d9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ DEVICE_PATH := device/motorola/rhode
 TARGET_BOOTLOADER_BOARD_NAME := rhode
 
 # Display
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 400
 
 # HIDL
 ODM_MANIFEST_SKUS += b bc d dc dn dnc n nc
