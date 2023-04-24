b18c19181064b9778c3afb1e491e60c5b917e2dd
Michael Bestas <mkbestas@lineageos.org>
2023-04-24 22:09:13 +0300

    rhode: Update odm sku manifests
    Change-Id: Ib7d84af46c72aebb48fd41ea451cc526e2928b81

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,15 +16,8 @@ TARGET_BOOTLOADER_BOARD_NAME := rhode
 TARGET_SCREEN_DENSITY := 400
 
 # HIDL
-ODM_MANIFEST_SKUS += b bc d dc dn dnc n nc
+ODM_MANIFEST_SKUS += b
 ODM_MANIFEST_B_FILES := $(DEVICE_PATH)/sku/manifest_b.xml
-ODM_MANIFEST_BC_FILES := $(DEVICE_PATH)/sku/manifest_bc.xml
-ODM_MANIFEST_D_FILES := $(DEVICE_PATH)/sku/manifest_d.xml
-ODM_MANIFEST_DC_FILES := $(DEVICE_PATH)/sku/manifest_dc.xml
-ODM_MANIFEST_DN_FILES := $(DEVICE_PATH)/sku/manifest_dn.xml
-ODM_MANIFEST_DNC_FILES := $(DEVICE_PATH)/sku/manifest_dnc.xml
-ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
-ODM_MANIFEST_NC_FILES := $(DEVICE_PATH)/sku/manifest_nc.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/ext_config/rhode-default.config
