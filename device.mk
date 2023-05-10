c895e12040f5a761942b8174ce93a75dcc577182
Michael Bestas <mkbestas@lineageos.org>
2023-05-10 22:45:32 +0300

    rhode: Decommonize NFC permissions
    Change-Id: I8e76e334fee6ac08d84b76f216f14500d22e3ad6

--- device.mk
+++ device.mk
@@ -31,6 +31,13 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom_ufs \
