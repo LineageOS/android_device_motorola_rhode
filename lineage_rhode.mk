8439d48606ccdb92251eca269f46dd6cfd9e2d3a
Michael Bestas <mkbestas@lineageos.org>
2023-10-31 23:12:51 +0200

    rhode: Update to S1SRS32.38-132-15
    Change-Id: Ibc51ad0b83be08c0811c6bd5fafc4bf7da668431

--- lineage_rhode.mk
+++ lineage_rhode.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := moto g52
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-BUILD_FINGERPRINT := "motorola/rhode_g/rhode:12/S1SRS32.38-132-14/c4430-03865f:user/release-keys"
+BUILD_FINGERPRINT := "motorola/rhode_g/rhode:12/S1SRS32.38-132-15/dac4a3-3e3fe:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=rhode_g \
-    PRIVATE_BUILD_DESC="rhode_g-user 12 S1SRS32.38-132-14 c4430-03865f release-keys"
+    PRIVATE_BUILD_DESC="rhode_g-user 12 S1SRS32.38-132-15 dac4a3-3e3fe release-keys"
