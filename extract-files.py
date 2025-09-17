21d5bfada1fff03cbcdbbea97868bdea9f1130ab
Michael Bestas <mkbestas@lineageos.org>
2025-09-17 23:23:33 +0300

    rhode: Switch to AIDL SEC NFC HAL
    Change-Id: I968b959d19306b667de2839526e3f0fa80887b71

--- extract-files.py
+++ extract-files.py
@@ -31,9 +31,6 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/etc/init/android.hardware.nfc@1.2-service.sec.rc': blob_fixup()
-        .regex_replace('sec', 'samsung')
-        .regex_replace('class hal', 'override\n    class hal'),
     (
         'vendor/lib64/camera/components/com.qti.node.gpu.so',
         'vendor/lib64/com.qti.feature2.gs.so',
