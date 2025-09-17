cf638426fa6798bcf63934a5019f811a35a73e7e
Michael Bestas <mkbestas@lineageos.org>
2025-09-17 23:23:33 +0300

    rhode: Switch to AIDL samsung NFC HAL
    Change-Id: I968b959d19306b667de2839526e3f0fa80887b71

--- device.mk
+++ device.mk
@@ -27,12 +27,13 @@ TARGET_SCREEN_WIDTH := 1080
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    init.oem.fingerprint2.sh \
-    init.mmi.overlay.rc
+    init.mmi.overlay.rc \
+    init.nfc.samsung.rc \
+    init.oem.fingerprint2.sh
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.samsung
+    android.hardware.nfc-service.samsung
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
